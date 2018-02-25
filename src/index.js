8eb5db9ef882ed1ac3f3a8acefa20a89a3e56f77
Denis <den107620@gmail.com>
2018-02-25 13:59:16 +0300

    Zero

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var zero = 0,
+      r = 0;
+  for (var i = 1; i <= number; i++){
+    ch = i;
+    do{
+
+      r = ch % 5;
+      if (r == 0){
+        zero++;
+        ch = ch / 5;
+      }
+    } while( r == 0);
+  };
+  return zero;
 }
